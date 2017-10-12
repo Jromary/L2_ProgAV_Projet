adfad5f3f46d92738df27193398de3fe7666e909
Julien Romary <romary16u@fst-ats-312-l04.fst.site.univ-lorraine.fr>
2017-10-12 15:52:11 +0200

    ajout du menu

---
+++ menu.c
@@ -0,0 +1,39 @@
+#include <SDL.h>
+
+#include "globals.h"
+#include "menu.h"
+#include "event.h"
+
+extern SDL_Surface *screen;
+extern int gameover_menu;
+
+
+void accueil(int argc, char *argv[])
+{
+    SDL_WM_SetCaption("PentoTrice", "PentoTrice");
+    SDL_EnableKeyRepeat(10, 100);
+
+    screen = SDL_SetVideoMode(screen_length, screen_height, 0, 0);
+    SDL_Surface *background, *temp;
+
+    temp = SDL_LoadBMP("Sprites/accueil_bg.bmp");
+	background = SDL_DisplayFormat(temp);
+	SDL_FreeSurface(temp);
+
+	int mouse_x, mouse_y;
+	char key[SDLK_LAST] = {0};
+
+	while (!gameover_menu)
+	{
+	    SDL_GetMouseState(&mouse_x, &mouse_y);
+		//update_events(key);
+		eventact_menu(key, mouse_x, mouse_y, &background);
+        SDL_BlitSurface(background, NULL, screen, NULL);
+
+
+		SDL_UpdateRect(screen, 0, 0, 0, 0);
+	}
+	SDL_FreeSurface(background);
+
+
+}
---
+++ menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <SDL.h>
+#include "globals.h"
+
+extern void accueil(int argc, char *argv[]);
+
+#endif /* MENU_H */
