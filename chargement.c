5da85367be3119195bf2e2a20b4bd9778fcab30a
Emmanuel Perrin-Deckert <perrinde2u@fst-ats-303-l16.fst.site.univ-lorraine.fr>
2017-10-23 16:04:19 +0200

    Correction d'erreur dans la detection d'erreur

--- chargement.c
+++ chargement.c
@@ -18,19 +18,74 @@ extern int nb_max_input_raw;
 extern Piece* tab_piece_all;
 extern int nb_max_input;
 
-int nb_lignes;
-int compteur_piece;
-int valide;
-char temp;
+int nb_lignes; // Utilisé dans compteur_ligne_input()
+int compteur_piece; // Utilisé dans compteur_ligne_input()
+char temp; // Utilisé dans compteur_ligne_input() - verification_input()
 
 
-/* Charge les entrées stockées dans "input.txt" dans les tableaux de pièces */
+/* Charge les entrées stockées dans "input.txt" dans les tableaux de pièces et fais des verifications adéquates */
 void load()
 {
-    /* Verification du fichier input */
-    verification_input();
-    /* Compteur de lignes dans le fichier input */
+    /* Test de validité : Fichier vide */
     compteur_ligne_input();
+    if(nb_lignes == 0)
+    {
+         printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
+         printf("ERREUR:\n >>> Fichier vide\n");
+         exit(0);
+    }
+
+    /* Test de validité : Pieces rectangulaires */
+    FILE *fichier_verif = fopen("input.txt", "r+");
+    int colonne, pieceActive, colonne_piece, estUnZero, COL;
+
+    if (fichier_verif != NULL)
+    {
+        colonne = 0;
+        colonne_piece = 0;
+        pieceActive = 0;
+        estUnZero = 0;
+        COL = 0;
+
+        do // Parcours du fichier char par char
+        {
+            temp = fgetc(fichier_verif);
+
+            if(!strcmp(&temp,"0"))
+            {
+                pieceActive = 0;
+                estUnZero = 1;
+            }
+            if(!strcmp(&temp,"\n"))
+            {
+                if(!pieceActive && !estUnZero) // Prend la reference de la piece
+                {
+                    colonne_piece = colonne; // nombre de colonne pour cette piece
+                    pieceActive = 1;
+                }
+                if(pieceActive && (colonne != colonne_piece)) // La piece n'a pas le même nombre de colonne sur chaque lignes
+                {
+                    COL = 1;
+                }
+                colonne = 0;
+                estUnZero = 0;
+            }
+            else{colonne++;}
+        }
+        while (temp != EOF );
+    }
+
+    if(COL)
+    {
+        printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
+        printf("ERREUR:\n >>> Une pièce n'a pas le même nombre de colonne sur chaque ligne\n");
+        SDL_Quit();
+        exit(0);
+    }
+
+    fclose(fichier_verif);
+
+
 
     FILE *fichier = fopen("input.txt", "r+");
     int* tab_raw_input = creation_tab_int(nb_lignes);
@@ -72,14 +127,35 @@ void load()
         tab_piece_all[nb_max_input] = const_Piece(larg, haut, 0, 0);
         int alea = (rand() % 3) + 1;
 
+        /* Test de validité : Piece vide */
         if(haut == 0) //Test si une piece est vide est dans le fichier
         {
             printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
             printf("ERREUR:\n >>> Chargement d'une pièce vide\n");
+            for (int m = 0; m < nb_max_input; m++)
+            {
+                free_piece(&tab_piece_all[i]);
+            }
+            fclose(fichier);
+            free(tab_raw_input);
             exit(0);
         }
 
-        for (j = 0; j < haut; j++)
+        /* Test de validité : Piece trop large */
+        if(larg > LARGEUR_PIECE_MAX) //Test si une piece est plus grande que le max
+        {
+            printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
+            printf("ERREUR:\n >>> Chargement d'une pièce d'une taille non conforme\n");
+            for (int m = 0; m < nb_max_input; m++)
+            {
+                free_piece(&tab_piece_all[i]);
+            }
+            fclose(fichier);
+            free(tab_raw_input);
+            exit(0);
+        }
+
+        for (j = 0; j < haut; j++) // Creation de piece
         {
             temp1 = tab_raw_input[deb+j];
             int countcol = 0;
@@ -99,13 +175,67 @@ void load()
                         break;
                     default:
                         printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
-                        printf("ERREUR:\n >>> Caractere numerique non conforme après verification\n");
+                        printf("ERREUR:\n >>> Caractere numerique non conforme\n");
                         exit(0);
                 }
                 countcol++;
             }
         }
+
         nb_max_input++;
+
+        // Piece crée
+        /* Test de validité : Ligne de vide */
+        int test_vide_ligne, test_vide_col;
+        for (int m=0; m<haut; m++)
+        {
+            test_vide_ligne = 1;
+            for(int p=0; p<larg; p++)
+            {
+                if(tab_piece_all[nb_max_input-1].grille[p][m].couleur != 10)// Ici, on n'a plus 8 et 1 mais un certain chiffre ou 10 pour le vide
+                {
+                    test_vide_ligne = 0;
+                }
+            }
+
+            if(test_vide_ligne)
+            {
+                printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
+                printf("ERREUR:\n >>> Presence d'une ligne de vide dans une pièce\n");
+                for (int nb = 0; nb < nb_max_input; nb++)
+                {
+                    free_piece(&tab_piece_all[nb]);
+                }
+                fclose(fichier);
+                free(tab_raw_input);
+                exit(0);
+            }
+        }
+        /* Test de validité : colonne de vide */
+        for (int m=0; m<larg; m++)
+        {
+            test_vide_col = 1;
+            for(int p=0; p<haut; p++)
+            {
+                if(tab_piece_all[nb_max_input-1].grille[m][p].couleur != 10)// Ici, on n'a plus 8 et 1 mais un certain chiffre ou 10 pour le vide
+                {
+                    test_vide_col = 0;
+                }
+            }
+
+            if(test_vide_col)
+            {
+                printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
+                printf("ERREUR:\n >>> Presence d'une colonne de vide dans une pièce\n");
+                for (int nb = 0; nb < nb_max_input; nb++)
+                {
+                    free_piece(&tab_piece_all[nb]);
+                }
+                fclose(fichier);
+                free(tab_raw_input);
+                exit(0);
+            }
+        }
     }
     fclose(fichier);
     free(tab_raw_input);
@@ -126,6 +256,7 @@ int compteMotif(int* tab_motif)
 }
 
 /* Compteur de lignes dans le fichier input */
+
 void compteur_ligne_input()
 {
     nb_lignes = 0;
@@ -150,85 +281,56 @@ void compteur_ligne_input()
 }
 
 /* Verification du fichier input */
+/*
 void verification_input()
 {
     FILE *fichier_verif = fopen("input.txt", "r+");
-    int colonne,compteurVide, NaN, OF,F1, NaVN; // Flags d'erreurs
-
-
-
+    int colonne, pieceActive, colonne_piece, estUnZero, COL;
 
     if (fichier_verif != NULL)
     {
         colonne = 0;
-        compteurVide = 0;
-        F1 = 0;
-        OF = 0;
-        NaN = 0;
-        NaVN = 0;// a faire
+        colonne_piece = 0;
+        pieceActive = 0;
+        estUnZero = 0;
+        COL = 0;
+
         do // Parcours du fichier char par char
         {
             temp = fgetc(fichier_verif);
-            if (isdigit(temp) || !strcmp(&temp, "\n")) // Test si le caractère lu est un nombre
+
+            if(!strcmp(&temp,"0"))
             {
-                if(!strcmp(&temp,"1")) // Compteur de case représentant le vide
-                {
-                    compteurVide++;
-                }
-            }
-            else
-            {
-                if(temp != EOF)
-                {
-                    NaN = 1;
-                }
-            }
-            if(colonne > LARGEUR_PIECE_MAX) // Test si la ligne de la piece est d'une taille conforme
-            {
-                OF = 1;
+                pieceActive = 0;
+                estUnZero = 1;
             }
             if(!strcmp(&temp,"\n"))
             {
-                //printf("col : %d - cv : %d\n",colonne,compteurVide);
-                if(compteurVide == colonne)
+                if(!pieceActive && !estUnZero) // Prend la reference de la piece
                 {
-                    valide = 0;
-                    F1 = 1;
+                    colonne_piece = colonne; // nombre de colonne pour cette piece
+                    pieceActive = 1;
+                }
+                if(pieceActive && (colonne != colonne_piece)) // La piece n'a pas le même nombre de colonne sur chaque lignes
+                {
+                    COL = 1;
                 }
                 colonne = 0;
-                compteurVide = 0;
+                estUnZero = 0;
             }
             else{colonne++;}
         }
-        while (temp != EOF );//&& valide);
+        while (temp != EOF );
     }
 
-        // Verification et fermeture anticipée si besoin
-        if(OF)
-        {
-            printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
-            printf("ERREUR:\n >>> Chargement d'une pièce d'une taille non conforme\n");
-        }
-        if(NaN)
-        {
-            printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
-            printf("ERREUR:\n >>> Caractere non numerique\n");
-        }
-        if(F1)
-        {
-            printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
-            printf("ERREUR:\n >>> Ligne uniquement composée de carrés vides\n");
-        }
-        if(NaVN)
-        {
-            printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
-            printf("ERREUR:\n >>> Caractère numérique non conforme (non 8 ou non 1)\n");
-        }
-        if(OF || NaN || F1 || NaVN)
-        {
-            exit(0);
-        }
+    if(COL)
+    {
+        printf("\nERREUR DANS LE CHARGEMENT DES PIECE DU FICHIER SOURCE\n");
+        printf("ERREUR:\n >>> Une pièce n'a pas le même nombre de colonne sur chaque ligne\n");
+    }
 
     fclose(fichier_verif);
+
 }
+*/
 
